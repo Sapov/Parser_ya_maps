f5bc6764df6e89b36dbe570d9d72a5a2527733a5
Sapov <sapov@mail.ru>
2026-03-30 23:57:44 +0300

    add undetected-chromedriver

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
-from parser.views import router as router_parser
+from parser import router as router_parser
 
 app = FastAPI()
 app.include_router(router_parser)
-
-@app.get('/hello')
-def get_hellow():
-    return 'hello'
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
 
 
 if __name__ == '__main__':
--- parser/views.py
+++ parser/views.py
@@ -6,3 +6,8 @@ router = APIRouter(prefix='/parser', tags=['parser'])
 @router.get('/')
 def hello():
     return 'HE'
+
+
+@router.post('/run/')
+def run_parser(category: str, city: str):
+    pass
