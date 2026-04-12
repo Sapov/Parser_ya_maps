897d29e3b53d01fad1be9606a8877fbe56d1ebf8
Sapov <sapov@mail.ru>
2026-04-12 23:03:43 +0300

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd /home/sasha/parser || exit
+
+echo "Pulling latest changes..."
+git pull
+
+echo "Running docker compose..."
+docker compose down
+docker compose pull
+docker compose up -d --build
+
+echo "Deploy completed!"
+docker compose ps
