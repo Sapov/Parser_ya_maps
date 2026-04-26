53c80468dc4a9ceebfff7e3378c99894984f310b
Sapov <sapov@mail.ru>
2026-04-26 14:52:31 +0300

    edit docker-compose.yml

--- core/config.py
+++ core/config.py
@@ -9,22 +9,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 class Settings(BaseSettings):
-    db_url: str = f"sqlite:///{BASE_DIR}/db.sqlite3"
-
-    async_bd_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
+    # db_url: str = f"sqlite:///{BASE_DIR}/db.sqlite3"
+    #
+    # async_bd_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
     db_echo: bool = False
     app_name: str = "Parser Yandex Map"
 
     # # PostgreSQL настройки
-    # DB_USER:str = os.getenv("DB_USER", "postgres")
-    # DB_PASSWORD:str = os.getenv("DB_PASSWORD", "postgres")
-    # DB_HOST:str = os.getenv("DB_HOST", "localhost")
-    # DB_PORT:str = os.getenv("DB_PORT", "5432")
-    # DB_NAME:str = os.getenv("DB_NAME", "parser_db")
-    # # Формируем URL для PostgreSQL
-    # db_url: str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    DB_USER:str = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD:str = os.getenv("DB_PASSWORD", "postgres")
+    DB_HOST:str = os.getenv("DB_HOST", "localhost")
+    DB_PORT:str = os.getenv("DB_PORT", "5432")
+    DB_NAME:str = os.getenv("DB_NAME", "parser_db")
+    # Формируем URL для PostgreSQL
+    db_url: str = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     # # Асинхронный URL для PostgreSQL
-    # async_bd_url: str = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    async_bd_url: str = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     # # Дополнительные настройки
     # db_echo: bool = False
     # db_pool_size: int = int(os.getenv("DB_POOL_SIZE", "20"))
